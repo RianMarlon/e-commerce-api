d54156a2ea37f9beb04fdae1baf8b5bd80a124de
Rian Marlon Sousa da Silva <rianmarlon95@gmail.com>
2022-04-23 18:38:13 -0300

    feat: add employees router in app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,14 +5,15 @@ import 'dotenv/config';
 import 'express-async-errors';
 
 import './database/connection';
-
 import { ErrorHandler } from './errors/error-handler';
+import { employeesRouter } from './routes/employees-routes';
 
 const app = express();
 const errorHandler = new ErrorHandler();
 
 app.use(express.json());
 app.use(helmet());
+app.use(employeesRouter);
 app.use(errorHandler.handleError);
 
 export { app };
