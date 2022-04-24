90b63f945c2b94442a6125fbfb2ea7db2ab34002
Rian Marlon Sousa da Silva <rianmarlon95@gmail.com>
2022-04-24 01:20:06 -0300

    feat: add route of employee update

--- src/routes/employees-routes.ts
+++ src/routes/employees-routes.ts
@@ -4,6 +4,7 @@ import { findAllEmployeesController } from '../controllers/employees/find-all/ma
 import { findByIdEmployeeController } from '../controllers/employees/find-by-id/main';
 import { typesEmployeeController } from '../controllers/employees/types-employee/main';
 import { createEmployeeController } from '../controllers/employees/create/main';
+import { updateEmployeeController } from '../controllers/employees/update/main';
 
 const employeesRouter = Router();
 
@@ -11,5 +12,6 @@ employeesRouter.get('/employees', findAllEmployeesController.handleRequest);
 employeesRouter.get('/employees/:id', findByIdEmployeeController.handleRequest);
 employeesRouter.get('/types-employee', typesEmployeeController.handleRequest);
 employeesRouter.post('/employees', createEmployeeController.handleRequest);
+employeesRouter.put('/employees/:id', updateEmployeeController.handleRequest);
 
 export { employeesRouter };
