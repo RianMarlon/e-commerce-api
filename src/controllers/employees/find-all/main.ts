676a5c054dbb22dd812b2216f531756b76e14874
Rian Marlon Sousa da Silva <rianmarlon95@gmail.com>
2022-04-23 21:35:59 -0300

    feat: add controller of find all employees

---
+++ src/controllers/employees/find-all/main.ts
@@ -0,0 +1,5 @@
+import { FindAllEmployeesController } from './find-by-all-controller';
+
+const findAllEmployeesController = new FindAllEmployeesController();
+
+export { findAllEmployeesController };
