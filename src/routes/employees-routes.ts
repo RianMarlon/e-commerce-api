eb8191fab8d044e80ebd05b22be3c7a0cc0b26b8
Rian Marlon Sousa da Silva <rianmarlon95@gmail.com>
2022-04-23 21:18:00 -0300

    feat: add route of find by employee id

--- src/routes/employees-routes.ts
+++ src/routes/employees-routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 
+import { findByIdEmployeeController } from '../controllers/employees/find-by-id/main';
 import { typesEmployeeController } from '../controllers/employees/types-employee/main';
 import { createEmployeeController } from '../controllers/employees/create/main';
 
 const employeesRouter = Router();
 
+employeesRouter.get('/employees/:id', findByIdEmployeeController.handleRequest);
 employeesRouter.get('/types-employee', typesEmployeeController.handleRequest);
 employeesRouter.post('/employees', createEmployeeController.handleRequest);
 
