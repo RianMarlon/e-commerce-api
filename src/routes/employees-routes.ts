c033f0eca55fd1e4303bd437829fb55e67ac7ccc
Rian Marlon Sousa da Silva <rianmarlon95@gmail.com>
2022-04-23 21:11:39 -0300

    feat: add route of create employee

--- src/routes/employees-routes.ts
+++ src/routes/employees-routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import { typesEmployeeController } from '../controllers/employees/types-employee/main';
+import { createEmployeeController } from '../controllers/employees/create/main';
 
 const employeesRouter = Router();
 
 employeesRouter.get('/types-employee', typesEmployeeController.handleRequest);
+employeesRouter.post('/employees', createEmployeeController.handleRequest);
 
 export { employeesRouter };
